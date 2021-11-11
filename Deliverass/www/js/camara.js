2e51f9a2cf1a8fbd7b98dba8c9b35d025993b9e9
AdrianSaut <adisautean@gmail.com>
2021-11-11 19:35:32 +0100

    Lector QR Actiu

--- Deliverass/www/js/camara.js
+++ Deliverass/www/js/camara.js
@@ -1,18 +1,17 @@
-document.getElementById("cameraTakePicture").addEventListener 
-   ("click", cameraTakePicture); 
+document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture); 
    function cameraTakePicture() { 
-    navigator.camera.getPicture(onSuccess, onFail, {  
-       quality: 50, 
-       correctOrientation: true,
-       destinationType: Camera.DestinationType.DATA_URL 
-    });  
+   navigator.camera.getPicture(onSuccess, onFail, {  
+      quality: 50, 
+      correctOrientation: true,
+      destinationType: Camera.DestinationType.DATA_URL 
+   });  
     
-    function onSuccess(imageData) { 
-       var image = document.getElementById('ImatgeCamara'); 
-       image.src = "data:image/jpeg;base64," + imageData; 
-    }  
+   function onSuccess(imageData) { 
+      var image = document.getElementById('ImatgeCamara'); 
+      image.src = "data:image/jpeg;base64," + imageData; 
+   }  
     
-    function onFail(message) { 
-       alert('Failed because: ' + message); 
-    } 
- }
+   function onFail(message) { 
+      alert('Failed because: ' + message); 
+   } 
+}
