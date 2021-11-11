0263d2082707b4dbbb1e503934beb060c6414dea
AdrianSaut <adisautean@gmail.com>
2021-11-11 18:02:13 +0100

    Nova seccio camara

--- Deliverass/www/js/camara.js
+++ Deliverass/www/js/camara.js
@@ -1,4 +1,18 @@
-document.addEventListener("click", obrirCamara, false);
-function obrirCamara() {
-    console.log(navigator.camera);
-}
+document.getElementById("cameraTakePicture").addEventListener 
+   ("click", cameraTakePicture); 
+   function cameraTakePicture() { 
+    navigator.camera.getPicture(onSuccess, onFail, {  
+       quality: 50, 
+       correctOrientation: true,
+       destinationType: Camera.DestinationType.DATA_URL 
+    });  
+    
+    function onSuccess(imageData) { 
+       var image = document.getElementById('ImatgeCamara'); 
+       image.src = "data:image/jpeg;base64," + imageData; 
+    }  
+    
+    function onFail(message) { 
+       alert('Failed because: ' + message); 
+    } 
+ }
